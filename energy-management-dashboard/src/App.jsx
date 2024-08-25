1b22bfc9e2a8e2081facce7d2ab602b82c134a3a
TahiryMariano <tahirymariano.htm@gmail.com>
2024-08-25 14:05:16 +0300

    feat(logi):change css to scss

--- energy-management-dashboard/src/App.jsx
+++ energy-management-dashboard/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
 
   return (
     <>
       <div className='app'>
-        <h1>Energy Management System Dashboard</h1>
+        <h1 className='font-bold'>Energy Management System Dashboard</h1>
       </div>
     </>
   )
