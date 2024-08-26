a0ee566154055ef2f68f9c8094c203a76bea45e5
TahiryMariano <tahirymariano.htm@gmail.com>
2024-08-26 12:22:03 +0300

    feat(login):configure default body style

--- energy-management-dashboard/src/App.jsx
+++ energy-management-dashboard/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.scss'
+import Login from './pages/login/Login'
 
 function App() {
 
   return (
     <>
       <div className='app'>
-        <h1 className='font-bold'>Energy Management System Dashboard</h1>
+        <Login/>
       </div>
     </>
   )
