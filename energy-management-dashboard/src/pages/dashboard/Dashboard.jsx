24d9c0e39e3409121b7fea9eec4e093767a1281e
TahiryMariano <tahirymariano.htm@gmail.com>
2024-08-26 23:58:45 +0300

    feat(dashboard):render content in dashboard

--- energy-management-dashboard/src/pages/dashboard/Dashboard.jsx
+++ energy-management-dashboard/src/pages/dashboard/Dashboard.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import imgNotification from "../../assets/notification.png";
-import imgUser from "../../assets/user.png";
 import imgAnalytics from "../../assets/carbon_analytics.png";
 import imgDashboard from "../../assets/dashboard.png";
 import imgLogout from "../../assets/logout.png";
-import imgLogo from "../../assets/logo.png";
+import imgLogo from "../../assets/logo-no-background.png";
 import styles from "./Dashboard.module.scss";
 import { useNavigate } from "react-router";
-import EnergyConsumptionChart from "../../components/EnergyConsumptionChart";
+import DashboardTab from "./tab/dashboard/DashboardTab";
+import Analyse from "./tab/analyse/Analyse"
+import Notification from "./tab/notification/Notification"
 
 function Dashboard() {
   const navigate = useNavigate();
+  const [activeTab, setActiveTab] = useState("dashboard");
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const renderContent = () => {
+    switch (activeTab) {
+      case "dashboard":
+        return <DashboardTab />;
+      case "analytics":
+        return <Analyse />;
+      case "notifications":
+        return <Notification />;
+      default:
+        return <Dashboard />;
+    }
+  };
 
   const handleLogout = (e) => {
     e.preventDefault();
@@ -27,19 +45,31 @@ function Dashboard() {
         <nav>
           <ul>
             <li>
-              <a href="#" className={styles.active}>
+              <a
+                href="#"
+                className={activeTab === "dashboard" ? styles.active : ""}
+                onClick={() => handleTabClick("dashboard")}
+              >
                 <img src={imgDashboard} alt="tableau de bord" />
                 Tableau de bord
               </a>
             </li>
             <li>
-              <a href="#">
+              <a
+                href="#"
+                className={activeTab === "analytics" ? styles.active : ""}
+                onClick={() => handleTabClick("analytics")}
+              >
                 <img src={imgAnalytics} alt="analyses" />
                 Analyses
               </a>
             </li>
             <li>
-              <a href="#">
+              <a
+                href="#"
+                className={activeTab === "notifications" ? styles.active : ""}
+                onClick={() => handleTabClick("notifications")}
+              >
                 <img src={imgNotification} alt="notifications" /> Notifications
               </a>
             </li>
@@ -53,43 +83,7 @@ function Dashboard() {
       </div>
 
       <div className={styles.content}>
-        <header>
-          <h1>Tableau de bord</h1>
-          <div className={styles.userProfile}>
-            <img src={imgUser} alt="User" />
-          </div>
-        </header>
-
-        <main>
-          <div className={styles.cards}>
-            <div className={styles.card}>
-              <h3>Puissance totale</h3>
-              <div className={styles.circleChart}>
-                <span>450W</span>
-              </div>
-            </div>
-
-            <div className={styles.card}>
-              <h3>Energies consommées</h3>
-              <div className={styles.meter}>
-                <span>18.0 Wh</span>
-              </div>
-              <p>Consommation: moyenne</p>
-            </div>
-
-            <div className={styles.card}>
-              <h3>Tension totale</h3>
-              <div className={styles.circleChart}>
-                <span>220V</span>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.chart}>
-            <h3>Consommation d'énergie par heure</h3>
-            <EnergyConsumptionChart />
-          </div>
-        </main>
+        {renderContent()}
       </div>
     </div>
   );
