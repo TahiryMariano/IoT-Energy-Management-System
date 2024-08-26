4495a06d6f6a8f10493bbc7fba92acf065be5ebe
TahiryMariano <tahirymariano.htm@gmail.com>
2024-08-26 15:53:19 +0300

    feat(dashboard):modify redirection in login

--- energy-management-dashboard/src/pages/login/Login.jsx
+++ energy-management-dashboard/src/pages/login/Login.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
-import styles from "./Login.module.scss"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Login.module.scss";
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [remember, setRemember] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,19 +16,24 @@ function Login() {
     console.log("Remember me:", remember);
 
     // Redirection vers le tableau de bord
-    window.location.href = "/dashboard.html";
-};
+    navigate("/dashboard");
+  };
 
   return (
-    <div className={styles.loginContainer}>
-      <div className={styles.leftSide}>
-            <h1>Bienvenue sur</h1>
-            <h2>Système de contrôle d'énergie</h2>
-            <p>Le Système de contrôle d'énergie est un projet électronique et informatique qui vise à surveiller, mesurer et optimiser la consommation d'énergie électrique.</p>
+    <div className={styles.loginWrapper}>
+      <div className={styles.loginContainer}>
+        <div className={styles.leftSide}>
+          <h1>Bienvenue sur</h1>
+          <h2>Système de contrôle d'énergie</h2>
+          <p>
+            Le Système de contrôle d'énergie est un projet électronique et
+            informatique qui vise à surveiller, mesurer et optimiser la
+            consommation d'énergie électrique.
+          </p>
         </div>
         <div className={styles.rightSide}>
-            <h2>Connexion</h2>
-            {/* <form action="dashboard.html" method="POST">
+          <h2>Connexion</h2>
+          {/* <form action="dashboard.html" method="POST">
                 <label for="email">E-mail</label>
                 <input type="email" id="email" name="email" required placeholder="johndoe@gmail.com"/>
                 
@@ -40,47 +47,48 @@ function Login() {
                 
                 <button type="submit">Se connecter</button>
             </form> */}
-            <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit}>
             <label htmlFor="email">E-mail</label>
-            <input 
-                type="email" 
-                id="email" 
-                name="email" 
-                required 
-                placeholder="johndoe@gmail.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              placeholder="johndoe@gmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
 
             <label htmlFor="password">Mot de passe</label>
-            <input 
-                type="password" 
-                id="password" 
-                name="password" 
-                required 
-                placeholder="Entrez votre mot de passe"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              placeholder="Entrez votre mot de passe"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
 
             <div className={styles.rememberForgot}>
-                <label>
-                    <input 
-                        type="checkbox" 
-                        name="remember" 
-                        checked={remember}
-                        onChange={(e) => setRemember(e.target.checked)}
-                    /> 
-                    Souviens-moi
-                </label>
-                <a href="#">Mots de passe oublié?</a>
+              <label>
+                <input
+                  type="checkbox"
+                  name="remember"
+                  checked={remember}
+                  onChange={(e) => setRemember(e.target.checked)}
+                />
+                Souviens-moi
+              </label>
+              <a href="#">Mots de passe oublié?</a>
             </div>
 
             <button type="submit">Se connecter</button>
-        </form>
+          </form>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
